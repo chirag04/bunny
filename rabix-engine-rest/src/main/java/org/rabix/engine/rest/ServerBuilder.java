a83489a405f9dc6d83301adc091e7b2272ea5df1
smiljevic <srdjan.miljevic@sbgenomics.com>
2017-11-13 22:40:53 +0100

    do not bind IntermediaryFilesService twice

--- rabix-engine-rest/src/main/java/org/rabix/engine/rest/ServerBuilder.java
+++ rabix-engine-rest/src/main/java/org/rabix/engine/rest/ServerBuilder.java
@@ -1,19 +1,12 @@
 package org.rabix.engine.rest;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.Set;
-
-import javax.servlet.DispatcherType;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.MultivaluedMap;
-
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Scopes;
+import com.google.inject.TypeLiteral;
+import com.google.inject.servlet.GuiceFilter;
+import com.google.inject.servlet.ServletModule;
+import com.squarespace.jersey2.guice.BootstrapUtils;
 import org.apache.commons.configuration.Configuration;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -40,23 +33,11 @@ import org.rabix.engine.rest.api.BackendHTTPService;
 import org.rabix.engine.rest.api.JobHTTPService;
 import org.rabix.engine.rest.api.impl.BackendHTTPServiceImpl;
 import org.rabix.engine.rest.api.impl.JobHTTPServiceImpl;
-import org.rabix.engine.service.BackendService;
-import org.rabix.engine.service.BootstrapService;
-import org.rabix.engine.service.BootstrapServiceException;
-import org.rabix.engine.service.IntermediaryFilesHandler;
-import org.rabix.engine.service.IntermediaryFilesService;
-import org.rabix.engine.service.JobService;
-import org.rabix.engine.service.SchedulerService;
+import org.rabix.engine.service.*;
 import org.rabix.engine.service.SchedulerService.SchedulerJobBackendAssigner;
 import org.rabix.engine.service.SchedulerService.SchedulerMessageCreator;
 import org.rabix.engine.service.SchedulerService.SchedulerMessageSender;
-import org.rabix.engine.service.impl.BackendServiceImpl;
-import org.rabix.engine.service.impl.BootstrapServiceImpl;
-import org.rabix.engine.service.impl.IntermediaryFilesServiceImpl;
-import org.rabix.engine.service.impl.JobReceiverImpl;
-import org.rabix.engine.service.impl.JobServiceImpl;
-import org.rabix.engine.service.impl.NoOpIntermediaryFilesServiceHandler;
-import org.rabix.engine.service.impl.SchedulerServiceImpl;
+import org.rabix.engine.service.impl.*;
 import org.rabix.engine.status.EngineStatusCallback;
 import org.rabix.engine.status.impl.DefaultEngineStatusCallback;
 import org.rabix.engine.stub.BackendStubFactory;
@@ -65,18 +46,23 @@ import org.rabix.transport.mechanism.TransportPlugin.ReceiveCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Injector;
-import com.google.inject.Scopes;
-import com.google.inject.TypeLiteral;
-import com.google.inject.servlet.GuiceFilter;
-import com.google.inject.servlet.ServletModule;
-import com.squarespace.jersey2.guice.BootstrapUtils;
+import javax.servlet.DispatcherType;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
 
 public class ServerBuilder {
 
   private final static Logger logger = LoggerFactory.getLogger(ServerBuilder.class);
-  
+
   private final static String ENGINE_PORT_KEY = "engine.port";
 
   private File configDir;
@@ -104,10 +90,9 @@ public class ServerBuilder {
             bind(DownloadService.class).to(NoOpDownloadServiceImpl.class).in(Scopes.SINGLETON);
             bind(UploadService.class).to(NoOpUploadServiceImpl.class).in(Scopes.SINGLETON);
             bind(JobHTTPService.class).to(JobHTTPServiceImpl.class);
-            bind(IntermediaryFilesService.class).to(IntermediaryFilesServiceImpl.class).in(Scopes.SINGLETON);
             bind(IntermediaryFilesHandler.class).to(NoOpIntermediaryFilesServiceHandler.class).in(Scopes.SINGLETON);
             bind(BackendHTTPService.class).to(BackendHTTPServiceImpl.class).in(Scopes.SINGLETON);
-            bind(new TypeLiteral<ReceiveCallback<Job>>(){}).to(JobReceiverImpl.class).in(Scopes.SINGLETON);             
+            bind(new TypeLiteral<ReceiveCallback<Job>>(){}).to(JobReceiverImpl.class).in(Scopes.SINGLETON);
             bind(WorkerStatusCallback.class).to(NoOpWorkerStatusCallback.class).in(Scopes.SINGLETON);
             Set<Class<BackendModule>> backendModuleClasses = ClasspathScanner.<BackendModule>scanSubclasses(BackendModule.class);
             for (Class<BackendModule> backendModuleClass : backendModuleClasses) {
@@ -132,15 +117,15 @@ public class ServerBuilder {
       logger.error("Failed to bootstrap engine", e);
       System.exit(-1);
     }
-    
+
     int enginePort = configuration.getInt(ENGINE_PORT_KEY);
     Server server = new Server(enginePort);
 
     ResourceConfig config = ResourceConfig.forApplication(new Application());
     config.register(CORSResponseFilter.class);
-    
+
     ServletContainer servletContainer = new ServletContainer(config);
-    
+
     ServletHolder sh = new ServletHolder(servletContainer);
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
